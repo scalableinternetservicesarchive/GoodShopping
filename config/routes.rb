e9835faa0e2c4fa733dbfe765defa07d6725e1eb
Rajan Saini <rajansaini@cs.ucsb.edu>
2021-11-18 22:34:14 -0800

    Create landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root "products#index" #TODO:: Add a home page, not products
+  root "landing_page#index"
   resources :products
   resources :manufacturers, only: %w[create new show]
-  resources :shoppers , only: %w[create new show index edit update]
+  resources :shoppers, only: %w[create new show index edit update]
   resources :carts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
