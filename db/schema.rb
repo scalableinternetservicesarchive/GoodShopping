0714119dc849c5d308061140be691ef3c97703b4
Shereen Elsayed <s_elsayed@ucsb.edu>
2021-11-21 00:54:15 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_055112) do
+ActiveRecord::Schema.define(version: 2021_11_20_201503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2021_11_12_055112) do
     t.text "product_information"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.binary "img"
     t.index ["manufacturer_id"], name: "index_products_on_manufacturer_id"
   end
 
