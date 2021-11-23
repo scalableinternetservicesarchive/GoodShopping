43404bb8c338114dbee9ac0290ab3c6ac05989c6
Shereen ElSayed <52331063+shereenElSayed@users.noreply.github.com>
2021-11-22 23:37:31 -0800

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
-    protect_from_forgery with: :null_session
+    protect_from_forgery with: :exception
 end
